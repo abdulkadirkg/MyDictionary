527eacbe1782fee982fa0f8773d805036fb01295
Abdulkadir KG <51646136+abdulkadirkg@users.noreply.github.com>
2021-02-14 15:11:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,14 +11,14 @@ class MyDictionary<TKey,TValue> where TKey : notnull
   List<(TKey, TValue)> list = new List<(TKey, TValue)>();
   public void Add(TKey key, TValue value)
   {
-  list.Add((key, value));
+    list.Add((key, value));
   }
 
   public void ShowAll()
   {
     for (int i = 0; i < list.Count; i++)
     {
-    Console.WriteLine("[" + list[i].Item1 + "," + list[i].Item2 + "]");
+      Console.WriteLine("[" + list[i].Item1 + "," + list[i].Item2 + "]");
     }
   }
 }
